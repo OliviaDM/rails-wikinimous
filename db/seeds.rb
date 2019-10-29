06d521d1bb699eb3eb0bc674dd0554bcd9bd3674
Olivia De Meyere <oliviademeyere@gmail.com>
2019-10-29 07:09:37 -0700

    ugly and functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  title = Faker::Books::Lovecraft.deity
+  content =Faker::Books::Lovecraft.paragraph
+  art = Article.new(title: title, content: content)
+  art.save
+end
